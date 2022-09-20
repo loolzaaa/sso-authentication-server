f7f0634b883f7ccbc507d2e37c82af573396ca3f
loolzaaa <loolzaaa@gmail.com>
2022-09-20 10:58:12 +0500

    Add hash and salt for admin default user in dev environment

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -33,9 +33,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
         hash text NOT NULL
     );
 
-    INSERT INTO users (login, config, enabled)
+    INSERT INTO users (login, salt, config, enabled)
         VALUES
-            ('admin', '{
+            ('admin', '1fca962b736b85bb727e9675ab1caf301d4f9b06b5c7585ac95dc7ce0b2349', '{
                 "passport": {
                     "roles": [
                         "ROLE_ADMIN"
@@ -44,5 +44,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
                 }
             }', true);
 
-    INSERT INTO hashes VALUES ('dummy');
+    INSERT INTO hashes VALUES ('5ac38d6a76b031e4d42459fd311cd0ef08bb0be92099aaf71f8472c6ccbefd8dd24bc315980345845f98912f50719f48ed6078c5a5efd4e83dc67ed16aed9b');
 EOSQL
