041b472312e8c37234975b47454912fb89a82bf2
loolzaaa <loolzaaa@gmail.com>
2022-09-22 22:38:18 +0500

    Update RefreshTokenCleanerTask

--- src/main/java/ru/loolzaaa/authserver/schedule/RefreshTokenCleanerTask.java
+++ src/main/java/ru/loolzaaa/authserver/schedule/RefreshTokenCleanerTask.java
@@ -6,28 +6,33 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import ru.loolzaaa.authserver.config.security.JWTUtils;
 
-import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 @Log4j2
 @RequiredArgsConstructor
 @Component
 @Profile("prod")
-public class RefreshTokenCleanerTask implements Callable<Integer> {
+public class RefreshTokenCleanerTask {
 
     private final JdbcTemplate jdbcTemplate;
 
-    @Scheduled(cron = "0 0 0 * * *")
-    @Override
-    public Integer call() {
-        int i = 0;
+    private final JWTUtils jwtUtils;
+
+    @Scheduled(initialDelay = 1, fixedDelay = 10, timeUnit = TimeUnit.MINUTES)
+    public void clean() {
+        int refreshTokenTtl = jwtUtils.getRefreshTokenTtl() / 1000 / 60 / 60;
         try {
-            // TODO: check expires, not now
-            i = jdbcTemplate.update("DELETE FROM refresh_sessions WHERE expires_in < now()");
-            log.info("Refresh tokens remove: [{}]", i);
+            if (refreshTokenTtl < 1) {
+                throw new IllegalArgumentException("Incorrect refresh token ttl: " + jwtUtils.getRefreshTokenTtl());
+            }
+            int i = jdbcTemplate.update(String.format("DELETE FROM refresh_sessions WHERE expires_in < (now() - interval '%d hour')", refreshTokenTtl));
+            if (i > 0) {
+                log.info("Refresh tokens remove: [{}]", i);
+            }
         } catch (Exception e) {
             log.error("Some problem with refresh token cleaner: ", e);
         }
-        return i;
     }
 }
