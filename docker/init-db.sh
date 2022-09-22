bbd85fec5c957045c3d8860d63f3296314be7550
loolzaaa <loolzaaa@gmail.com>
2022-09-22 15:02:48 +0500

    Update base initializing for development

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -51,7 +51,14 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
                     ],
                     "someSetting":true
                 }
+            }', true),
+            ('user', 'f735f1e6322ad9319abe78bc02ac06f8ad7a8673a81eb33a8525c0ff359fc8', '{
+                "passport": {
+                    "credentials_exp": 2524608000000
+                }
             }', true);
 
-    INSERT INTO hashes VALUES ('5ac38d6a76b031e4d42459fd311cd0ef08bb0be92099aaf71f8472c6ccbefd8dd24bc315980345845f98912f50719f48ed6078c5a5efd4e83dc67ed16aed9b');
+    INSERT INTO hashes VALUES
+      ('5ac38d6a76b031e4d42459fd311cd0ef08bb0be92099aaf71f8472c6ccbefd8dd24bc315980345845f98912f50719f48ed6078c5a5efd4e83dc67ed16aed9b'),
+      ('21a2a7ef7b6979be41b9d6872942bf6df2daf1f8fd547cc9da729b84c47442209948e96786a9a66a9983b0b032d119c6e6f34bc222c1ebf0bd36fbfc0e0790');
 EOSQL
