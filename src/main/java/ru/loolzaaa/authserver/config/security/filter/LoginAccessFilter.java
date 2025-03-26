28017449e622d304ae4246b9e52d5e3e78aa1c2f
Korsakov A.N. (20211) <loolzaaa@gmail.com>
2025-03-26 11:35:54 +0500

    Fix login page access
    From Spring Security 6 default dispatcher types for security chain - all types, so need to skip LoginAccessFilter if security chain processed twice or more times

--- src/main/java/ru/loolzaaa/authserver/config/security/filter/LoginAccessFilter.java
+++ src/main/java/ru/loolzaaa/authserver/config/security/filter/LoginAccessFilter.java
@@ -1,6 +1,8 @@
 package ru.loolzaaa.authserver.config.security.filter;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +14,7 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.UrlUtils;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.loolzaaa.authserver.config.WebConfig;
 import ru.loolzaaa.authserver.config.security.CookieName;
@@ -26,7 +28,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @RequiredArgsConstructor
-public class LoginAccessFilter extends GenericFilterBean {
+public class LoginAccessFilter extends OncePerRequestFilter {
 
     private final SsoServerProperties ssoServerProperties;
 
@@ -44,13 +46,11 @@ public class LoginAccessFilter extends GenericFilterBean {
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest servletRequest = (HttpServletRequest) req;
-        HttpServletResponse servletResponse = (HttpServletResponse) resp;
-
+    protected void doFilterInternal(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain chain)
+            throws ServletException, IOException {
         String uriWithoutContextPath = servletRequest.getRequestURI().substring(servletRequest.getContextPath().length());
-        String appParameter = req.getParameter("app");
-        String continueParameter = req.getParameter("continue");
+        String appParameter = servletRequest.getParameter("app");
+        String continueParameter = servletRequest.getParameter("continue");
         if (isAuthenticatedUserGoesToLoginPage(uriWithoutContextPath)) {
             logger.debug("Already authenticated user with login path detected");
             // Set already authenticated attribute for late controller processing
@@ -91,8 +91,8 @@ public class LoginAccessFilter extends GenericFilterBean {
             try {
                 String continueUrl = new String(Base64.getUrlDecoder().decode(continueParameter)).replaceAll("[\r\n]", "_");
                 if (isValidUrl(continueUrl)) {
-                    RequestDispatcher dispatcher = req.getRequestDispatcher(ssoServerProperties.getLoginPage());
-                    dispatcher.forward(req, resp);
+                    RequestDispatcher dispatcher = servletRequest.getRequestDispatcher(ssoServerProperties.getLoginPage());
+                    dispatcher.forward(servletRequest, servletResponse);
                     return;
                 }
                 logger.warn("Continue parameter is not absolute url or empty: " + continueUrl);
